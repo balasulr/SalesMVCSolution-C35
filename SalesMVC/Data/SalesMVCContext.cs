ab29677355d7e6ed815cc72c525345c070783943
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-02-24 15:46:23 -0500

    Add project files.

---
+++ SalesMVC/Data/SalesMVCContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesMVC.Models;
+
+namespace SalesMVC.Data
+{
+    public class SalesMVCContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Orderline> Orderlines { get; set; }
+
+        public SalesMVCContext (DbContextOptions<SalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+
+        }
+        
+    }
+}
---
+++ SalesMVC/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesMVC.Models {
+    public class Customer {
+
+        public int Id { get; set; }
+        [Required, StringLength(30)]
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal Sales { get; set; }
+        [Required, StringLength(2)]
+        public string StateCode { get; set; } = "OH";
+
+    }
+}
